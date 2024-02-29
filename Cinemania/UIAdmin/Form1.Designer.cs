c61d26127c33496f52ff19da0b6dbe9bfd176754
Marylene <baeyens.ma@gmail.com>
2024-02-29 20:09:37 +0100

    Ajout de Chaine de cinemas dans l'UI et amélioration du code, débuging des messages d'avertissements

--- Cinemania/UIAdmin/Form1.Designer.cs
+++ Cinemania/UIAdmin/Form1.Designer.cs
@@ -39,8 +39,6 @@
             this.labelTitre = new System.Windows.Forms.Label();
             this.dgvChaine = new System.Windows.Forms.DataGridView();
             this.MenuChaine = new System.Windows.Forms.ContextMenuStrip(this.components);
-            this.ajouterChaineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
-            this.modifierChaineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             this.supprimerChaineToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
             ((System.ComponentModel.ISupportInitialize)(this.dgvCine)).BeginInit();
             this.MenuCinema.SuspendLayout();
@@ -51,9 +49,10 @@
             // btGetCinemas
             // 
             this.btGetCinemas.BackColor = System.Drawing.SystemColors.Info;
-            this.btGetCinemas.Location = new System.Drawing.Point(665, 90);
+            this.btGetCinemas.Location = new System.Drawing.Point(760, 120);
+            this.btGetCinemas.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btGetCinemas.Name = "btGetCinemas";
-            this.btGetCinemas.Size = new System.Drawing.Size(114, 40);
+            this.btGetCinemas.Size = new System.Drawing.Size(130, 53);
             this.btGetCinemas.TabIndex = 0;
             this.btGetCinemas.Text = "Tous les Cinemas";
             this.btGetCinemas.UseVisualStyleBackColor = false;
@@ -65,47 +64,51 @@
             this.dgvCine.BackgroundColor = System.Drawing.Color.Salmon;
             this.dgvCine.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             this.dgvCine.ContextMenuStrip = this.MenuCinema;
-            this.dgvCine.Location = new System.Drawing.Point(290, 198);
+            this.dgvCine.Location = new System.Drawing.Point(331, 264);
+            this.dgvCine.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvCine.Name = "dgvCine";
+            this.dgvCine.RowHeadersWidth = 51;
             this.dgvCine.RowTemplate.Height = 25;
             this.dgvCine.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvCine.Size = new System.Drawing.Size(498, 231);
+            this.dgvCine.Size = new System.Drawing.Size(569, 308);
             this.dgvCine.TabIndex = 1;
             // 
             // MenuCinema
             // 
+            this.MenuCinema.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.MenuCinema.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
             this.ajouterCinemaToolStripMenuItem,
             this.modifierCinemaToolStripMenuItem,
             this.supprimerCinémaToolStripMenuItem});
             this.MenuCinema.Name = "MenuCinema";
-            this.MenuCinema.Size = new System.Drawing.Size(174, 70);
+            this.MenuCinema.Size = new System.Drawing.Size(202, 76);
             // 
             // ajouterCinemaToolStripMenuItem
             // 
             this.ajouterCinemaToolStripMenuItem.Name = "ajouterCinemaToolStripMenuItem";
-            this.ajouterCinemaToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.ajouterCinemaToolStripMenuItem.Size = new System.Drawing.Size(201, 24);
             this.ajouterCinemaToolStripMenuItem.Text = "Ajouter Cinéma";
             // 
             // modifierCinemaToolStripMenuItem
             // 
             this.modifierCinemaToolStripMenuItem.Name = "modifierCinemaToolStripMenuItem";
-            this.modifierCinemaToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.modifierCinemaToolStripMenuItem.Size = new System.Drawing.Size(201, 24);
             this.modifierCinemaToolStripMenuItem.Text = "Modifier Cinéma";
             // 
             // supprimerCinémaToolStripMenuItem
             // 
             this.supprimerCinémaToolStripMenuItem.Name = "supprimerCinémaToolStripMenuItem";
-            this.supprimerCinémaToolStripMenuItem.Size = new System.Drawing.Size(173, 22);
+            this.supprimerCinémaToolStripMenuItem.Size = new System.Drawing.Size(201, 24);
             this.supprimerCinémaToolStripMenuItem.Text = "Supprimer Cinéma";
             this.supprimerCinémaToolStripMenuItem.Click += new System.EventHandler(this.supprimerCinémaToolStripMenuItem_Click);
             // 
             // btUpdate
             // 
             this.btUpdate.BackColor = System.Drawing.SystemColors.Info;
-            this.btUpdate.Location = new System.Drawing.Point(542, 90);
+            this.btUpdate.Location = new System.Drawing.Point(619, 120);
+            this.btUpdate.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.btUpdate.Name = "btUpdate";
-            this.btUpdate.Size = new System.Drawing.Size(99, 40);
+            this.btUpdate.Size = new System.Drawing.Size(113, 53);
             this.btUpdate.TabIndex = 3;
             this.btUpdate.Text = "Upd Cinemas";
             this.btUpdate.UseVisualStyleBackColor = false;
@@ -116,9 +119,9 @@
             this.labelTitre.AutoSize = true;
             this.labelTitre.Font = new System.Drawing.Font("Trebuchet MS", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
             this.labelTitre.ForeColor = System.Drawing.Color.Maroon;
-            this.labelTitre.Location = new System.Drawing.Point(38, 21);
+            this.labelTitre.Location = new System.Drawing.Point(43, 28);
             this.labelTitre.Name = "labelTitre";
-            this.labelTitre.Size = new System.Drawing.Size(365, 35);
+            this.labelTitre.Size = new System.Drawing.Size(456, 43);
             this.labelTitre.TabIndex = 4;
             this.labelTitre.Text = "Administration des cinemas";
             // 
@@ -127,52 +130,43 @@
             this.dgvChaine.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dgvChaine.BackgroundColor = System.Drawing.Color.Salmon;
             this.dgvChaine.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvChaine.Location = new System.Drawing.Point(22, 198);
+            this.dgvChaine.Location = new System.Drawing.Point(25, 264);
+            this.dgvChaine.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.dgvChaine.Name = "dgvChaine";
+            this.dgvChaine.RowHeadersWidth = 51;
             this.dgvChaine.RowTemplate.Height = 25;
             this.dgvChaine.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.dgvChaine.Size = new System.Drawing.Size(241, 231);
+            this.dgvChaine.Size = new System.Drawing.Size(275, 308);
             this.dgvChaine.TabIndex = 5;
+            this.dgvChaine.CellEndEdit += new System.Windows.Forms.DataGridViewCellEventHandler(this.dgvChaine_CellEndEdit);
             this.dgvChaine.SelectionChanged += new System.EventHandler(this.dgvChaines_SelectionChanged);
             // 
             // MenuChaine
             // 
+            this.MenuChaine.ImageScalingSize = new System.Drawing.Size(20, 20);
             this.MenuChaine.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
-            this.ajouterChaineToolStripMenuItem,
-            this.modifierChaineToolStripMenuItem,
             this.supprimerChaineToolStripMenuItem});
             this.MenuChaine.Name = "MenuChaine";
-            this.MenuChaine.Size = new System.Drawing.Size(170, 70);
-            // 
-            // ajouterChaineToolStripMenuItem
-            // 
-            this.ajouterChaineToolStripMenuItem.Name = "ajouterChaineToolStripMenuItem";
-            this.ajouterChaineToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
-            this.ajouterChaineToolStripMenuItem.Text = "Ajouter Chaine";
-            // 
-            // modifierChaineToolStripMenuItem
-            // 
-            this.modifierChaineToolStripMenuItem.Name = "modifierChaineToolStripMenuItem";
-            this.modifierChaineToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
-            this.modifierChaineToolStripMenuItem.Text = "Modifier Chaine";
+            this.MenuChaine.Size = new System.Drawing.Size(197, 28);
             // 
             // supprimerChaineToolStripMenuItem
             // 
             this.supprimerChaineToolStripMenuItem.Name = "supprimerChaineToolStripMenuItem";
-            this.supprimerChaineToolStripMenuItem.Size = new System.Drawing.Size(169, 22);
+            this.supprimerChaineToolStripMenuItem.Size = new System.Drawing.Size(196, 24);
             this.supprimerChaineToolStripMenuItem.Text = "Supprimer Chaine";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(224)))), ((int)(((byte)(192)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(914, 600);
             this.Controls.Add(this.dgvChaine);
             this.Controls.Add(this.labelTitre);
             this.Controls.Add(this.btUpdate);
             this.Controls.Add(this.dgvCine);
             this.Controls.Add(this.btGetCinemas);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Form1";
             this.Text = "Administration";
             ((System.ComponentModel.ISupportInitialize)(this.dgvCine)).EndInit();
@@ -196,8 +190,6 @@
         private ToolStripMenuItem modifierCinemaToolStripMenuItem;
         private ToolStripMenuItem supprimerCinémaToolStripMenuItem;
         private ContextMenuStrip MenuChaine;
-        private ToolStripMenuItem ajouterChaineToolStripMenuItem;
-        private ToolStripMenuItem modifierChaineToolStripMenuItem;
         private ToolStripMenuItem supprimerChaineToolStripMenuItem;
     }
 }
--- Cinemania/UIAdmin/Form1.cs
+++ Cinemania/UIAdmin/Form1.cs
@@ -1,5 +1,6 @@
 using Models;
 using Newtonsoft.Json;
+using System.ComponentModel;
 using System.Net.Http.Json;
 using System.Security.Policy;
 using System.Text;
@@ -8,6 +9,7 @@ namespace UIAdmin
 {
     public partial class Form1 : Form
     {
+        private static readonly HttpClient client = new HttpClient();
         private int _currentChaineId;
         public Form1()
         {
@@ -18,21 +20,22 @@ namespace UIAdmin
 
         async void LoadChaines()
         {
-            string sUrlServeur = "https://localhost:7013";
-            using (HttpClient client = new HttpClient())
-            {
-                HttpResponseMessage response = await client.GetAsync(sUrlServeur + "/api/Admin/Chaine");
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7013/api/Admin/Chaine");
 
-                if (response.IsSuccessStatusCode)
+            if (response.IsSuccessStatusCode)
                 {
                     var responseContent = await response.Content.ReadAsStringAsync();
-                    var chaines = JsonConvert.DeserializeObject<List<ChaineDTO>>(responseContent);
+                    var chaines = JsonConvert.DeserializeObject<BindingList<ChaineDTO>>(responseContent);
                     dgvChaine.DataSource = chaines;
 
                     dgvChaine.Columns["ch_id"].Visible = false;
                     dgvChaine.Columns["ch_nom"].HeaderText = "Chaine de Cinéma";
+
+                    foreach (DataGridViewColumn column in dgvChaine.Columns)
+                    {
+                        column.ReadOnly = false;
+                    }
                 }
-            }
         }
 
         void CustomizeDataGridView()
@@ -54,14 +57,11 @@ namespace UIAdmin
                 LoadCinemasByChaine(chaineId);
             }
         }
-
         async void LoadCinemasByChaine(int chaineId)
         {
             _currentChaineId = chaineId;
-            string sUrlServeur = "https://localhost:7013";
-            using (HttpClient client = new HttpClient())
             {
-                HttpResponseMessage response = await client.GetAsync($"{sUrlServeur}/api/Admin/CinemasByChaine/{chaineId}");
+                HttpResponseMessage response = await client.GetAsync("https://localhost:7013/api/Admin/CinemasByChaine/" + chaineId); 
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -80,23 +80,27 @@ namespace UIAdmin
             }
         }
 
-    async private void btGetCinemas_Click(object sender, EventArgs e)
+        async private void btGetCinemas_Click(object sender, EventArgs e)
         {
-            string sUrlServeur = "https://localhost:7013";
-            using (HttpClient client = new HttpClient())
+            HttpResponseMessage response = await client.GetAsync("https://localhost:7013/api/Admin/Cinemas");
+
+            if (response.IsSuccessStatusCode)
             {
-                HttpResponseMessage response = await client.GetAsync(sUrlServeur + "/api/Admin/Cinemas" );
-                
-                if (response.IsSuccessStatusCode)
+                string responseContent = await response.Content.ReadAsStringAsync();
+                if (!string.IsNullOrEmpty(responseContent))
                 {
-                    string responseContent = await response.Content.ReadAsStringAsync();
-                    List<CinemasDTO> data = JsonConvert.DeserializeObject<List<CinemasDTO>>(responseContent);
-                    dgvCine.DataSource = data;
+                    var data = JsonConvert.DeserializeObject<List<CinemasDTO>>(responseContent);
+                    dgvCine.DataSource = data ?? new List<CinemasDTO>(); // l'opérateur ?? fournira une liste vide en cas de null
                 }
                 else
                 {
-                    string responseContent = await response.Content.ReadAsStringAsync();
+                    dgvCine.DataSource = new List<CinemasDTO>();
                 }
+            }
+            else
+            {
+                string responseContent = await response.Content.ReadAsStringAsync();
+                MessageBox.Show("Impossible de charger les données des cinémas.");
             }
         }
 
@@ -111,10 +115,9 @@ namespace UIAdmin
             oCinema.ci_ch_id = 1;
 
             JsonContent content = JsonContent.Create(oCinema);
-
-            using (HttpClient client = new HttpClient())
+            
             {
-                HttpResponseMessage response = await client.PostAsync(sUrlServeur + "/api/Admin/Cinemas", content);
+                HttpResponseMessage response = await client.GetAsync("https://localhost:7013/api/Admin/Cinemas");
 
                 if (response.IsSuccessStatusCode)
                 {
@@ -126,17 +129,14 @@ namespace UIAdmin
                 }
             }
         }
-
         private async void supprimerCinémaToolStripMenuItem_Click(object sender, EventArgs e)
         {
             if (dgvCine.CurrentRow != null)
             {
-                int cinemaId = Convert.ToInt32(dgvCine.CurrentRow.Cells["ci_id"].Value); 
-                string sUrlServeur = "https://localhost:7013";
+                int cinemaId = Convert.ToInt32(dgvCine.CurrentRow.Cells["ci_id"].Value);
 
-                using (HttpClient client = new HttpClient())
                 {
-                    HttpResponseMessage response = await client.DeleteAsync($"{sUrlServeur}/api/Admin/Cinemas/{cinemaId}");
+                    HttpResponseMessage response = await client.GetAsync("https://localhost:7013/api/Admin/Cinemas" + cinemaId);
 
                     if (response.IsSuccessStatusCode)
                     {
@@ -147,6 +147,35 @@ namespace UIAdmin
                     {
                         MessageBox.Show("La suppression a échoué.");
                     }
+                }
+            }
+        }
+        private async Task AjouterNouvelleChaine(AjoutChaineDTO nouvelleChaine)
+        {
+                StringContent content = new StringContent(JsonConvert.SerializeObject(nouvelleChaine), Encoding.UTF8, "application/json");
+                HttpResponseMessage response = await client.PostAsync("https://localhost:7013/api/Admin/Chaines", content);
+
+                if (response.IsSuccessStatusCode)
+                {
+                    LoadChaines();
+                }
+                else
+                {
+                    MessageBox.Show("Erreur lors de l'ajout de la nouvelle chaîne. Veuillez réessayer.");
+                }   
+        }
+        private async void dgvChaine_CellEndEdit(object sender, DataGridViewCellEventArgs e)
+        {
+            if (dgvChaine.Columns[e.ColumnIndex].Name == "ch_nom")
+            {
+                var row = dgvChaine.Rows[e.RowIndex];
+                var nouvelleChaineNom = row.Cells["ch_nom"].Value?.ToString();
+
+                // Vérifier si la cellule éditée contient une valeur non nulle et non vide
+                if (!string.IsNullOrWhiteSpace(nouvelleChaineNom))
+                {
+                    AjoutChaineDTO nouvelleChaine = new AjoutChaineDTO { ch_nom = nouvelleChaineNom };
+                    await AjouterNouvelleChaine(nouvelleChaine);
                 }
             }
         }
